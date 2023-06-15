f239e827c34ca765ce23660ffabc937d6796912f
PACI <mail@pa.ci>
2023-06-15 18:33:10 +0800

    bug fix

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ wget --no-check-certificate -O memoryCheck.sh https://raw.githubusercontent.com/
 ### 气球驱动（Balloon）
 通过virtio_balloon驱动实现动态调整Guest与Host的可用内存空间。Balloon的工作原理是在虚拟机中安装一个kmod，KVM宿主机内存不足，会根据virtio_balloon判断哪些内存页面可以被回收，然后virtio_balloon将这些内存占用，返回给宿主机使用。
 
-root用户执行`rmmod virtio_balloon`可以关闭virtio_balloon
+root用户执行`rmmod virtio_balloon`可以关闭virtio_balloon。事实上，大多数使用Proxmox VE的厂商都会开启virtio_balloon进行超售。
 
 ### KSM（Kernel Samepage Merging 内存合并）
 KSM是一种内存合并技术，它可以在KVM中实现内存共享，从而节省内存空间。是Linux kernel的一种内存共享机制，在2.6.32版本引入，用于合并具有相同内容的物理主存页面以减少页面冗余。在Kernel中KSM会定期扫描用户注册的内存区域，当有相同的页面就会将其合并，并用一个添加到页表中的新页面来代替原来的页面。当需要修改时，复制新的内存页，再做修改(将其标记为 copy-on-write)
--- memoryCheck.sh
+++ memoryCheck.sh
@@ -13,11 +13,12 @@ memSize=`free -m | awk '/Mem/ {print $2}'`
 speed=`dd if=/dev/zero of=/dev/null bs=1M count=$memSize 2>&1 | awk '/copied/ {print $(NF-1)}'`
 speed=`echo $speed | awk '{printf("%.0f\n",$1)}'`
 echo "内存io速度: $speed GB/s"
+echo ""
 if [ $speed -lt 10 ]; then
     echo -e "\033[31mSWAP超售!\033[0m"
     echo "超售! 内存io速度低于 10 GB/s，存在SWAP超售可能"
 else
-    echo "内存io速度正常，未使用SWAP超售"
+    echo "未使用SWAP超售，内存io速度正常"
 fi
 
 # Check virtio_balloon oversold
@@ -26,7 +27,7 @@ if lsmod | grep virtio_balloon > /dev/null; then
     echo -e "\033[31mballoon超售!\033[0m"
     echo "存在virtio_balloon模块，使用气球驱动Balloon超售内存"
 else
-    echo "不存在virtio_balloon模块，未使用气球驱动Balloon超售"
+    echo "未使用气球驱动Balloon超售，不存在virtio_balloon模块"
 fi
 
 # Check Kernel Samepage Merging (KSM) oversold
@@ -35,5 +36,5 @@ if [ `cat /sys/kernel/mm/ksm/run` == 1 ]; then
     echo -e "\033[31mKSM超售!\033[0m"
     echo "Kernel Samepage Merging状态为1，使用KSM超售内存"
 else
-    echo "Kernel Samepage Merging状态正常，未使用KSM超售"
+    echo "未使用KSM超售，Kernel Samepage Merging状态正常，"
 fi
