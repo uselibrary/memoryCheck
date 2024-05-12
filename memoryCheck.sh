2a2df249b8a42a38a8c0e79641894fad44b44b1f
0x648 <xzai0313@qq.com>
2024-05-13 02:09:55 +0800

    提升脚本结果易读性

--- memoryCheck.sh
+++ memoryCheck.sh
@@ -1,40 +1,41 @@
 #!/usr/bin/env bash
 
-# check if is utf-8, if not, set it
-# if [ `locale charmap` != 'UTF-8' ]; then
-#     export LC_ALL=en_US.UTF-8
-# fi
+echo -e "\033[33m内存超售检测开始\033[0m"
+echo -e "\033[0m====================\033[0m"
 
-echo "内存超售检测开始......"
-echo ""
-
-# Check SWAP oversold
+# 检查是否使用了 SWAP 超售内存
+echo -e "\033[36m检查是否使用了 SWAP 超售内存\033[0m"
 memSize=`free -m | awk '/Mem/ {print $2}'`
 speed=`dd if=/dev/zero of=/dev/null bs=1M count=$memSize 2>&1 | awk '{print $(NF-1)}' | awk 'END {print}' | awk -F '，' '{print $NF}'`
 speed=`echo $speed | awk '{printf("%.0f\n",$1)}'`
-echo "内存io速度: $speed GB/s"
-echo ""
+echo -e "\033[34m内存 IO 速度: $speed GB/s\033[0m"
 if [ $speed -lt 10 ]; then
-    echo -e "\033[31mSWAP超售!\033[0m"
-    echo "内存io速度低于 10 GB/s，存在SWAP超售可能"
+    echo -e "\033[31m内存 IO 速度低于 10 GB/s\033[0m"
+    echo -e "\033[31m可能存在 SWAP 超售内存\033[0m"
 else
-    echo "未使用SWAP超售，内存io速度正常"
+    echo -e "\033[32m内存 IO 速度正常\033[0m"
+    echo -e "\033[32m未使用 SWAP 超售内存\033[0m"
 fi
+echo -e "\033[0m====================\033[0m"
 
-# Check virtio_balloon oversold
-echo ""
+# 检查是否使用了气球驱动 Balloon 超售内存
+echo -e "\033[36m检查是否使用了 气球驱动 Balloon 超售内存\033[0m"
 if lsmod | grep virtio_balloon > /dev/null; then
-    echo -e "\033[31mballoon超售!\033[0m"
-    echo "存在virtio_balloon模块，使用气球驱动Balloon超售内存"
+    echo -e "\033[31m存在 virtio_balloon 模块\033[0m"
+    echo -e "\033[31m可能使用了 气球驱动 Balloon 超售内存\033[0m"
 else
-    echo "未使用气球驱动Balloon超售，不存在virtio_balloon模块"
+    echo -e "\033[32m不存在 virtio_balloon 模块\033[0m"
+    echo -e "\033[32m未使用 气球驱动 Balloon 超售内存\033[0m"
 fi
+echo -e "\033[0m====================\033[0m"
 
-# Check Kernel Samepage Merging (KSM) oversold
-echo ""
+# 检查是否使用了 Kernel Samepage Merging (KSM) 超售内存
+echo -e "\033[36m检查是否使用了 Kernel Samepage Merging (KSM) 超售内存\033[0m"
 if [ `cat /sys/kernel/mm/ksm/run` == 1 ]; then
-    echo -e "\033[31mKSM超售!\033[0m"
-    echo "Kernel Samepage Merging状态为1，使用KSM超售内存"
+    echo -e "\033[31mKernel Samepage Merging 状态为 1\033[0m"
+    echo -e "\033[31m可能使用了 Kernel Samepage Merging (KSM) 超售内存\033[0m"
 else
-    echo "未使用KSM超售，Kernel Samepage Merging状态正常，"
+    echo -e "\033[32mKernel Samepage Merging 状态正常\033[0m"
+    echo -e "\033[32m未使用 Kernel Samepage Merging (KSM) 超售内存\033[0m"
 fi
+echo -e "\033[0m====================\033[0m"
